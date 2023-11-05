f966fce17b7770832e73c01d5e2fae95221181a2
kevin pinsag <kpinsag@gmail.com>
2023-11-05 00:34:33 -0500

    2do commit cursoangular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { EmpleadosComponent } from './empleados/empleados.component';
+import { EmpleadoComponent } from './empleado/empleado.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmpleadosComponent
+    EmpleadosComponent,
+    EmpleadoComponent
   ],
   imports: [
     BrowserModule
